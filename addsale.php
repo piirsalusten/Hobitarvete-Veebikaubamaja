e8769db0b97ef11e3a074a965b3c6a6882623bef
piirsalusten <piirsalu.sten@mail.com>
2018-12-09 21:11:38 +0200

    test2

--- addsale.php
+++ addsale.php
@@ -116,7 +116,6 @@
     if (isset($_POST["type"]) && !empty($_POST["type"])){ //kui on määratud ja pole tühi
         $productType = intval($_POST["type"]);
         echo $productType;
-        echo $type;
     } else {
         $productTypeError = " (Palun vali sobiv!) Määramata!";
     }
